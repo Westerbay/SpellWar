4d7be7b801e1cb84b5b4ab855818fd6fa7d1d6c7
Westerbay <westerbay@outlook.com>
2025-02-12 22:03:09 +0100

    Missing file

---
+++ include/spellwar/world/Platform.hpp
@@ -0,0 +1,28 @@
+/*
+ * Wester
+ * This code is open source and free to use.
+ * 
+ * You are free to copy, modify, and distribute this file without restriction.
+ * No warranties are provided, and any use of this code is at your own risk.
+ */
+
+
+#ifndef __PLATFORM_H__
+#define __PLATFORM_H__
+
+#include <wgame/wgame.hpp>
+
+
+using namespace wgame;
+
+class Platform : public GameObject {
+public:
+    Platform(const Cuboid & platform);
+    void update() override;
+    void render() override;
+private:
+    Cuboid _platform;
+};
+
+#endif
+
---
+++ src/spellwar/world/Platform.cpp
@@ -0,0 +1,23 @@
+/*
+ * Wester
+ * This code is open source and free to use.
+ * 
+ * You are free to copy, modify, and distribute this file without restriction.
+ * No warranties are provided, and any use of this code is at your own risk.
+ */
+
+
+#include <spellwar/world/Platform.hpp>
+
+
+Platform::Platform(const Cuboid & platform) : _platform(platform) {}
+
+void Platform::update() {
+    
+}
+
+void Platform::render() {
+    ColorDrawer colorDrawer;
+    colorDrawer.drawCuboid(_platform, {1.0f, 0.0f, 0.0f});
+}
+
