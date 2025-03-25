5277e82cc2354f1120363860dfe5a7ba91d0d3b8
Westerbay <westerbay@outlook.com>
2025-03-25 18:19:36 +0100

    Ajust map

--- include/spellwar/world/World.hpp
+++ include/spellwar/world/World.hpp
@@ -17,10 +17,10 @@
 
 #include <wgame/wgame.hpp>
 
-#define MAP_SIZE Vector3D(100.0f, 75.0f, 100.0f)
-#define MIN_PLATFORM_SIZE Vector3D(10.0f, 1.0f, 10.0f)
-#define MAX_PLATFORM_SIZE Vector3D(30.0f, 1.0f, 30.0f)
-#define MAX_NUMBER_OF_PLATFORMS 30
+#define MAP_SIZE Vector3D(80.0f, 80.0f, 80.0f)
+#define MIN_PLATFORM_SIZE Vector3D(12.0f, 1.0f, 12.0f)
+#define MAX_PLATFORM_SIZE Vector3D(36.0f, 1.0f, 36.0f)
+#define MAX_NUMBER_OF_PLATFORMS 20
 #define FAR_PLANE 600.0f
 
 
